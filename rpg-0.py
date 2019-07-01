b9e3122a4bdb78c50647147d97d7a912e996d700
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-07-01 15:53:22 -0400

    Initial Commit

--- rpg-0.py
+++ rpg-0.py
@@ -1,21 +1,33 @@
 """
 In this simple RPG game, the hero fights the goblin. He has the options to:
-
 1. fight goblin
 2. do nothing - in which case the goblin will attack him anyway
 3. flee
-
 """
 
-def main():
+'''def main():
     hero_health = 10
     hero_power = 5
     goblin_health = 6
-    goblin_power = 2
+    goblin_power = 2'''
+class Hero():
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+hunter = Hero(10, 5)
 
-    while goblin_health > 0 and hero_health > 0:
-        print("You have %d health and %d power." % (hero_health, hero_power))
-        print("The goblin has %d health and %d power." % (goblin_health, goblin_power))
+class Goblin():
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+gunter = Goblin(6,2)
+
+
+
+
+while gunter.health > 0 and hunter.health > 0:
+        print("You have %d health and %d power." % (hunter.health, hunter.power))
+        print("The goblin has %d health and %d power." % (gunter.health, gunter.power))
         print()
         print("What do you want to do?")
         print("1. fight goblin")
@@ -25,9 +37,9 @@ def main():
         user_input = input()
         if user_input == "1":
             # Hero attacks goblin
-            goblin_health -= hero_power
-            print("You do %d damage to the goblin." % hero_power)
-            if goblin_health <= 0:
+            gunter.health -= hunter.power
+            print("You do %d damage to the goblin." % hunter.power)
+            if gunter.health <= 0:
                 print("The goblin is dead.")
         elif user_input == "2":
             pass
@@ -37,11 +49,9 @@ def main():
         else:
             print("Invalid input %r" % user_input)
 
-        if goblin_health > 0:
+        if gunter.health > 0:
             # Goblin attacks hero
-            hero_health -= goblin_power
-            print("The goblin does %d damage to you." % goblin_power)
-            if hero_health <= 0:
+            hunter.health -= gunter.power
+            print("The goblin does %d damage to you." % gunter.power)
+            if hunter.health <= 0:
                 print("You are dead.")
-
-main()
