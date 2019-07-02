4234ca2494920a26d196c84cdb67f7702d127960
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-07-02 11:33:53 -0400

    changes

--- rpg-0.py
+++ rpg-0.py
@@ -1,15 +1,10 @@
-"""
-In this simple RPG game, the hero fights the goblin. He has the options to:
-1. fight goblin
-2. do nothing - in which case the goblin will attack him anyway
-3. flee
-"""
 
-'''def main():
-    hero_health = 10
-    hero_power = 5
-    goblin_health = 6
-    goblin_power = 2'''
+import random
+
+
+
+
+
 class Character():
     def __init__(self, health, power):
         self.health = health
@@ -20,37 +15,53 @@ class Character():
        return self.health > 0
     def print_status(self):
         print(self.health) 
-           
 
-
-
-
-"""class Goblin():
+class Medic(Character):
     def __init__(self, health, power):
-        self.health = health
-        self.power = power
-    def attack(self,enemy):
-        enemy.health -= self.power
-    def alive(self):
-       return self.health > 0
-    def print_status(self):
-        print(self.health) """
-    
+        super().__init__(health, power)
+    def recover(self):
+        heal = random.randint(1,5)
+        if heal == 5:
+            self.health += 2
 
-hunter = Character(10, 5)
-gunter = Character(6,2)
-#zombie = Character(999, 2)
+class Hero(Character):
+    def __init__(self, health, power):
+        super().__init__(health, power)
+    def attack(self, enemy):
+        crit = random.randint(1,5)
+        if crit == 1:
+            enemy.health -= self.power * 2
+            print("Critical Hit!")
+        else:
+            enemy.health -= self.power
 
-#hunter.attack(gunter)
-#print(gunter.__dict__)
+class Phantom(Character):
+    def __init__(self, health, power):
+        super().__init__(health, power)
+    def ghost(self):
+        phase = random.randint(1,10)
+        if phase == 1:
+            self.health == 1
 
+class Undead(Character):
+    def __init__(self, health, power):
+        super().__init__(health, power)
+    def NeverDie(self):
+        if self.health == 0:
+            self.health += 666
 
+       
+hunter = Hero(100, 5)
+medic = Medic(25, 5)
+shadow = Phantom(25, 1)
+runner = Character(20, 2)
 
-
-
-
+gunter = Character(25,2)
+wizard = Character(40, 4)
+zombie = Undead(1, 2)
 
 while gunter.alive() == True and hunter.alive()== True:
+
         print("You have %d health and %d power." % (hunter.health, hunter.power))
         print("The goblin has %d health and %d power." % (gunter.health, gunter.power))
         print()
@@ -61,7 +72,7 @@ while gunter.alive() == True and hunter.alive()== True:
         print("> ",)
         user_input = input()
         if user_input == "1":
-            # Hero attacks goblin
+            # Hero attacks goblin            
             gunter.health -= hunter.power
             print("You do %d damage to the goblin." % hunter.power)
             if gunter.health <= 0:
@@ -80,3 +91,37 @@ while gunter.alive() == True and hunter.alive()== True:
             print("The goblin does %d damage to you." % gunter.power)
             if hunter.alive() == False:
                 print("You are dead.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################################################
+# Muted code#
+
+"""class Goblin():
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+    def attack(self,enemy):
+        enemy.health -= self.power
+    def alive(self):
+       return self.health > 0
+    def print_status(self):
+        print(self.health) """
+
